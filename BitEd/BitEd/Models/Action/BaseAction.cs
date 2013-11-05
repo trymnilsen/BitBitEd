bf8f7c30d167c2d58f8e6e0d63c79a7b01a7d36e
Trym Nilsen <niltry10@nith.no>
2013-11-06 00:13:54 +0100

    Inspector start

---
+++ BitEd/BitEd/Models/Action/BaseAction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitEd.Models.Action
+{
+    public enum ExecutionPriority
+    {
+        Pre,
+        Normal,
+        Post
+    }
+    class BaseAction
+    {
+        public ExecutionPriority ExecutionPriority { get; set; }
+        public ObservableCollection<ActionProperty> ActionProperties { get; set; }
+
+        public BaseAction()
+            : this(ExecutionPriority.Normal) { }
+
+        public BaseAction(ExecutionPriority priority)
+        {
+            ActionProperties = new ObservableCollection<ActionProperty>();
+            ExecutionPriority = priority;
+        }
+    }
+}
