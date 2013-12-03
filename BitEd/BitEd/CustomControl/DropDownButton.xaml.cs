effe7aaa0c07cb31043ebec5f1e9714e1f5edfdf
Trym Nilsen <niltry10@nith.no>
2013-12-04 00:57:39 +0100

    dropdown

---
+++ BitEd/BitEd/CustomControl/DropDownButton.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BitEd.CustomControl
+{
+    /// <summary>
+    /// Interaction logic for DropDownButton.xaml
+    /// </summary>
+    public partial class DropDownButton : UserControl
+    {
+        public DropDownButton()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ClickButton.ContextMenu.IsOpen = true;
+            e.Handled = true;
+        }
+    }
+}
