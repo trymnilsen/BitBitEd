350c8c8fa873ac14a324e141eeda74ee18abad02
Trym Nilsen <trym.nilsen@gmail.com>
2013-12-07 22:16:44 +0100

    added some properties to the sprite template

--- BitEd/BitEd/Models/Entity/EntitySprite.cs
+++ BitEd/BitEd/Models/Entity/EntitySprite.cs
@@ -15,6 +15,7 @@ namespace BitEd.Models.Entity
         private int centerX;
         private int centerY;
         private int ID;
+        private int size;
 
         public BitmapImage ImageSource
         {
@@ -23,7 +24,41 @@ namespace BitEd.Models.Entity
                 return imageSource;
             }
         }
-
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+        }
+        public int Width
+        {
+            get
+            {
+                return width;
+            }
+        }
+        public int CenterX
+        {
+            get
+            {
+                return centerX;
+            }
+        }
+        public int CenterY
+        {
+            get
+            {
+                return centerY;
+            }
+        }
+        public int Size
+        {
+            get
+            {
+                return size;
+            }
+        }
         public override bool Validate
         {
             get { return imageSource == null ? false : true; }
@@ -33,6 +68,11 @@ namespace BitEd.Models.Entity
             :base(node,EntityType.Sprite,name)
         {
             this.imageSource = imageSource;
+            width = imageSource.PixelWidth;
+            height = imageSource.PixelHeight;
+            centerX = width / 2;
+            centerY = height / 2;
+            size = (int)imageSource.StreamSource.Length;
         }
         
     }
