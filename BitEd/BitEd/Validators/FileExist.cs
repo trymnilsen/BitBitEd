c79f0340f3d32c49571e913abd41fab9683ead79
Trym Nilsen <trym.nilsen@gmail.com>
2013-12-07 22:49:52 +0100

    filebox control

---
+++ BitEd/BitEd/Validators/FileExist.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace BitEd.Validators
+{
+    class FileExist:ValidationRule
+    {
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            string FilePath = Convert.ToString(value);
+            if (File.Exists(FilePath))
+            {
+                return new ValidationResult(true, null);
+            }
+            else
+            {
+                return new ValidationResult(false, "Please enter a valid filepath");
+            }
+            
+        }
+    }
+}
