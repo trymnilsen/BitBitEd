ec5a4323761dc6ef52b10b87763c881f363119be
Trym Nilsen <trym.nilsen@gmail.com>
2013-12-14 02:19:46 +0100

    No action added

---
+++ BitEd/BitEd/Converter/GreaterThanConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BitEd.Converter
+{
+    public class GreaterThanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            int valueToTest = System.Convert.ToInt32(value);
+            int threshold = System.Convert.ToInt32(parameter);
+            return (valueToTest > threshold);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException("Cannot convert from true false to value");
+        }
+    }
+}
