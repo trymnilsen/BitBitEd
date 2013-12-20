0333b1d46886db337071e02e946f4541a4e960c6
Trym Nilsen <trym.nilsen@gmail.com>
2013-12-20 21:16:31 +0100

    Starting on screen

---
+++ BitEd/BitEd/Models/Entity/EntityScreen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitEd.Models.Entity
+{
+    public class EntityScreen
+    {
+        public ObservableCollection<EntityScreenObjectInstance> Instances { get; private set; }
+
+        public EntityScreen()
+        {
+
+        }
+    }
+}
